e5687c8c7af22f60f3cfdc3d2753807bb583ef6e
Ban <h93910@163.com>
2016-02-24 17:05:42 +0800

    2016.02.24

--- src/com/ban/graduationproject/MainActivity.java
+++ src/com/ban/graduationproject/MainActivity.java
@@ -1,26 +1,38 @@
 package com.ban.graduationproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.ban.graduationproject.commbase.ComBase;
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
+import com.ban.graduationproject.commbase.SimpleChange;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
-import com.nineoldandroids.animation.Animator;
-import com.nineoldandroids.animation.Animator.AnimatorListener;
 import com.zxing.activity.CaptureActivity;
 
 public class MainActivity extends MyBaseActivity {
 	private SlidingMenu slidingMenu;
 
+	private TextView mTitleView;
+	private ImageView mMenuBtnView;
+	private ImageView mSetBtnView;
+	private LinearLayout layout;
+	private FragmentManager fragmentManager;
 	private Intent goToScan;
 
 	private static int REQUESTCODE_LOCK = 0;// 放车请求码
 	private static int REQUESTCODE_UNLOCK = 1;// 拿车请求码
+	private static int REQUESTCODE_RECHARGE = 2;// 充值请求码
+
+	private SimpleChange change;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +46,41 @@ public class MainActivity extends MyBaseActivity {
 		slidingMenu.setBehindWidth(ComBase.dp2pix(150));// 设置弹出多少
 		slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
 		slidingMenu.setMenu(R.layout.item_sliding_menu);
+
+		layout = (LinearLayout) findViewById(R.id.main_fragment);
+		fragmentManager = getSupportFragmentManager();
+
+		goToRecord();
 	}
 
 	@Override
 	public void initView() {
+		mMenuBtnView = (ImageView) findViewById(R.id.main_menu);
+		mSetBtnView = (ImageView) findViewById(R.id.main_set);
+		mTitleView = (TextView) findViewById(R.id.title_string);
 
+		List<TextView> textViewSet = new ArrayList<TextView>();
+		textViewSet.add((TextView) findViewById(R.id.main_recording_text));
+		textViewSet.add((TextView) findViewById(R.id.main_recharge_text));
+		textViewSet.add((TextView) findViewById(R.id.main_personal_text));
+
+		List<ImageView> imageViewSet = new ArrayList<ImageView>();
+		imageViewSet.add((ImageView) findViewById(R.id.main_recording_pic));
+		imageViewSet.add((ImageView) findViewById(R.id.main_recharge_pic));
+		imageViewSet.add((ImageView) findViewById(R.id.main_personal_pic));
+
+		List<Integer> defaultPic = new ArrayList<Integer>();
+		defaultPic.add(R.drawable.main_recording);
+		defaultPic.add(R.drawable.main_recharge);
+		defaultPic.add(R.drawable.main_personal);
+
+		List<Integer> changePic = new ArrayList<Integer>();
+		changePic.add(R.drawable.main_recording_fouse);
+		changePic.add(R.drawable.main_recharge_focus);
+		changePic.add(R.drawable.main_personal_focus);
+
+		change = new SimpleChange(this, textViewSet, R.color.main_disable,
+				R.color.main_color2, imageViewSet, defaultPic, changePic);
 	}
 
 	@Override
@@ -49,11 +91,20 @@ public class MainActivity extends MyBaseActivity {
 				int id = v.getId();
 				if (id == R.id.main_menu) {
 					slidingMenu.showMenu(!slidingMenu.isMenuShowing());
+				} else if (id == R.id.main_recording_layout) {
+					goToRecord();
+				} else if (id == R.id.main_recharge_layout) {
+					goToRecharge();
+				} else if (id == R.id.main_personal_layout) {
+					goToPersonal();
 				}
 			}
 		};
 
-		findViewById(R.id.main_menu).setOnClickListener(listener);
+		mMenuBtnView.setOnClickListener(listener);
+		findViewById(R.id.main_recording_layout).setOnClickListener(listener);
+		findViewById(R.id.main_recharge_layout).setOnClickListener(listener);
+		findViewById(R.id.main_personal_layout).setOnClickListener(listener);
 	}
 
 	@Override
@@ -82,6 +133,16 @@ public class MainActivity extends MyBaseActivity {
 	}
 
 	/**
+	 * 充值的处理
+	 * 
+	 * @param v
+	 */
+	public void recharge(View v) {
+		goToScan = new Intent(MainActivity.this, CaptureActivity.class);
+		startActivityForResult(goToScan, REQUESTCODE_RECHARGE);
+	}
+
+	/**
 	 * 跳转到设置
 	 * 
 	 * @param v
@@ -89,6 +150,70 @@ public class MainActivity extends MyBaseActivity {
 	public void goToSet(View v) {
 		Intent temp = new Intent(this, SetAcitivity.class);
 		startActivity(temp);
+	}
+
+	/**
+	 * 是否显示标题栏两边按钮
+	 */
+	private void showHeadBtn(boolean show) {
+		int v = 0;
+		if (show)
+			v = View.VISIBLE;
+		else
+			v = View.GONE;
+		mMenuBtnView.setVisibility(v);
+		mSetBtnView.setVisibility(v);
+		slidingMenu.setBehindWidth(show == true ? ComBase.dp2pix(150) : 0);
+	}
+
+	/**
+	 * 跳转到拿放车记录
+	 */
+	public void goToRecord() {
+		change.executeChange(0);
+		mTitleView.setText(R.string.main_recording);
+		showHeadBtn(true);
+
+		fragmentManager.beginTransaction()
+				.replace(layout.getId(), new RecordingFragment()).commit();
+	}
+
+	/**
+	 * 跳转到充值记录
+	 */
+	public void goToRecharge() {
+		change.executeChange(1);
+		mTitleView.setText(R.string.main_recharge);
+		showHeadBtn(true);
+
+		fragmentManager.beginTransaction()
+				.replace(layout.getId(), new RechargeFragment()).commit();
+
+	}
+
+	/**
+	 * 跳转到用户管理
+	 */
+	public void goToPersonal() {
+		change.executeChange(2);
+		mTitleView.setText(R.string.main_personal);
+		showHeadBtn(true);
+
+		fragmentManager.beginTransaction()
+				.replace(layout.getId(), new PersonalFragment()).commit();
+
+	}
+
+	/**
+	 * 跳转到用户添加页面
+	 */
+	public void goToPersonalAdd() {
+		mTitleView.setText(R.string.main_personal_add);
+		showHeadBtn(false);
+
+		fragmentManager.beginTransaction()
+				.replace(layout.getId(), new PersonalAddFragment()).commit();
+
 	}
 
 	/**
@@ -106,6 +231,8 @@ public class MainActivity extends MyBaseActivity {
 				Log.d("Ban", getString(R.string.menu_lock));
 			} else if (arg0 == REQUESTCODE_UNLOCK) {
 				Log.d("Ban", getString(R.string.menu_unlock));
+			} else if (arg0 == REQUESTCODE_RECHARGE) {
+				Log.d("Ban", getString(R.string.menu_recharge));
 			}
 
 			Log.d("Ban", "车的ID：" + result);
--- src/com/ban/graduationproject/commbase/MyApplication.java
+++ src/com/ban/graduationproject/commbase/MyApplication.java
@@ -1,6 +1,10 @@
 package com.ban.graduationproject.commbase;
 
+import com.ban.graduationproject.database.BaseDAO;
+import com.ban.graduationproject.database.ManagerDAO;
 import com.ban.graduationproject.database.MySampleDate;
+import com.ban.graduationproject.entity.Manager;
+import com.ban.graduationproject.entity.User;
 
 import android.app.Application;
 import android.content.Context;
@@ -8,7 +12,7 @@ import android.util.Log;
 
 public class MyApplication extends Application {
 	private Context context;
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -21,9 +25,12 @@ public class MyApplication extends Application {
 		// 初始化键值对处理类
 		MySampleDate.getInstance(context, "setting");
 
-
 		// 设置分辨率
 		ComBase.DPI = getResources().getDisplayMetrics().density;
 		Log.d("系统分辨率", ComBase.DPI + "");
+
+		// 初始化数据库
+		ComBase.initDB(context);
+		new ManagerDAO(context);
 	}
 }
